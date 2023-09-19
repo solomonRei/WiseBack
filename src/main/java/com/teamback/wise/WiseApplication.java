f33e41bdd9bb27dde49921d521c53e96ff422f84
DumbravaM <42375617+dumbrava2001@users.noreply.github.com>
2023-09-19 19:03:51 +0300

    Add initial project dependencies

---
+++ src/main/java/com/teamback/wise/WiseApplication.java
@@ -0,0 +1,13 @@
+package com.teamback.wise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamback/wise/WiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamback.wise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
