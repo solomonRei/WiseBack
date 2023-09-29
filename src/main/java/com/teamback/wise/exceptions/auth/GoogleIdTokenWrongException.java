644c8643eb23f23ef89324d2028cab413ad688bf
Eduard <dz6class@mail.ru>
2023-09-29 18:11:36 +0300

    Task #4106: added google dependency, implemented Google Auth

---
+++ src/main/java/com/teamback/wise/exceptions/auth/GoogleIdTokenWrongException.java
@@ -0,0 +1,9 @@
+package com.teamback.wise.exceptions.auth;
+
+public class GoogleIdTokenWrongException extends RuntimeException {
+
+        public GoogleIdTokenWrongException(String message) {
+            super(String.format("Google ID token is wrong: %s", message));
+        }
+
+}
--- src/main/java/com/teamback/wise/services/GoogleTokenVerifierService.java
+++ src/main/java/com/teamback/wise/services/GoogleTokenVerifierService.java
@@ -1,11 +1,20 @@
 package com.teamback.wise.services;
 
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import com.teamback.wise.configurations.GoogleConfigurationProperties;
+import com.teamback.wise.exceptions.auth.FailedGoogleAuthException;
+import com.teamback.wise.exceptions.auth.GoogleIdTokenWrongException;
 import com.teamback.wise.models.responses.GoogleUserResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,9 +26,35 @@ public class GoogleTokenVerifierService {
     public GoogleUserResponse verifyGoogleId(String googleId) {
         log.info("Verifying Google ID: " + googleId);
 
-        return GoogleUserResponse.builder()
-                .username("seo")
-                .email("test@mail.ru")
-                .build();
+        try {
+
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+                    .setAudience(Collections.singletonList(googleConfigurationProperties.getClientId()))
+                    .build();
+
+            GoogleIdToken idToken = verifier.verify(googleId);
+            if (idToken != null) {
+                Payload payload = idToken.getPayload();
+                String userId = payload.getSubject();
+                String email = payload.getEmail();
+
+                log.info("Writing Google Info: " + userId + " " + email);
+
+                return GoogleUserResponse.builder()
+                        .username(userId)
+                        .email(email)
+                        .build();
+
+            } else {
+                log.error("Invalid ID token.");
+
+                //TODO: use GlobalExceptionHandler
+                throw new GoogleIdTokenWrongException(googleId);
+            }
+
+        } catch (Exception e) {
+            log.error("Error authenticating user with Google ID.");
+            throw new FailedGoogleAuthException(e.getMessage());
+        }
     }
 }
