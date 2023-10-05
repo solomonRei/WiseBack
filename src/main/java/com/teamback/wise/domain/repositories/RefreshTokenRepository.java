55a9471e25fb1b28bba3162f51b9d79e514b3365
tutaf <the.tutaf@yandex.kz>
2023-10-05 14:06:27 +0300

    bugfix: edit RefreshTokenRepository

--- src/main/java/com/teamback/wise/domain/repositories/RefreshTokenRepository.java
+++ src/main/java/com/teamback/wise/domain/repositories/RefreshTokenRepository.java
@@ -3,6 +3,7 @@ package com.teamback.wise.domain.repositories;
 import com.teamback.wise.domain.entities.RefreshTokenEntity;
 import com.teamback.wise.domain.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,5 +12,6 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, String> {
     Optional<RefreshTokenEntity> findByToken(String token);
 
-    Optional<RefreshTokenEntity> findByUser(UserEntity user);
+    @Modifying
+    void deleteByToken(String token);
 }
