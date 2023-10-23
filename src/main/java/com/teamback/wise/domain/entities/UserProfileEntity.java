369ee241072a3c54d2a5f2f54efbce5745cc1805
Andrei Cernisov <andrei.cernisov@isa.utm.md>
2023-10-23 12:08:55 +0300

    task #4109: create User Profile table, entity and repository

---
+++ src/main/java/com/teamback/wise/domain/entities/UserProfileEntity.java
@@ -0,0 +1,79 @@
+package com.teamback.wise.domain.entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_profiles")
+public class UserProfileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "profile_picture_url")
+    @URL
+    private String profilePictureUrl;
+
+    @Column(name = "youtube_channel_id", unique = true, nullable = false)
+    @NotNull
+    private String youtubeChannelId;
+
+    @Column(name = "youtube_channel_name", nullable = false)
+    @NotNull
+    private String youtubeChannelName;
+
+    @Column(name = "youtube_first_video_date")
+    private LocalTime youtubeFirstVideoDate;
+
+    @Column(name = "youtube_registration_date")
+    private LocalTime youtubeRegistrationDate;
+
+    @Column(name = "youtube_handle", unique = true)
+    private String youtubeHandle;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalTime updatedAt;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private UserEntity user;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalTime.now();
+    }
+}
